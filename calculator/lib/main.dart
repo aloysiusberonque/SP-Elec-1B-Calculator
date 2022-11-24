50d78ff176c4d701495037da1ade427d7bd2cd6a
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-11-24 18:41:34 +0800

    Implemented Design

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/buttons.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,8 +19,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+              flex: 2,
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return MyButton(
+                          buttonText: buttons[index],
+                          color: Colors.green,
+                          textColor: Colors.white);
+                    } else if (index == 1) {
+                      return MyButton(
+                          buttonText: buttons[index],
+                          color: Colors.red,
+                          textColor: Colors.white);
+                    } else {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.deepPurple
+                            : Colors.deepPurple[50],
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.deepPurple,
+                      );
+                    }
+                  })),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
   }
 }
