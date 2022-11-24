78cff8d615d804d905c441cc498cfffba355c155
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-11-24 20:06:38 +0800

    Implemented Evaluation

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() => runApp(MyApp());
 
@@ -94,6 +95,7 @@ class _HomePageState extends State<HomePage> {
                           buttonTapped: () {
                             setState(() {
                               userQuestion = '';
+                              userAnswer = '0';
                             });
                           },
                           buttonText: buttons[index],
@@ -113,6 +115,20 @@ class _HomePageState extends State<HomePage> {
                           color: Colors.red,
                           textColor: Colors.white);
                     }
+
+                    // Equal Button
+                    else if (index == buttons.length - 1) {
+                      return MyButton(
+                          buttonTapped: () {
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                          buttonText: buttons[index],
+                          color: Colors.deepPurple,
+                          textColor: Colors.white);
+                    }
+
                     // Rest of the buttons
                     else {
                       return MyButton(
@@ -141,5 +157,18 @@ class _HomePageState extends State<HomePage> {
       return true;
     }
     return false;
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer = eval.toString();
   }
 }
